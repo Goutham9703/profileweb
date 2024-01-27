88f9c477a2e5ff3f04c5e1d3fb575b3f0d0fbe48
root <root@ip-172-31-32-187.ap-south-1.compute.internal>
2024-01-27 08:24:30 +0000

    src final update

---
+++ src/main/webapp/WEB-INF/views/old/js/siteJs.js
@@ -0,0 +1,80 @@
+	//  var myval;
+	// function Mycall() {
+	// 	myval = setTimeout(page, 300);
+	// }
+	// function page() {
+	// 	$("#loader-wrapper").css("display", "none");
+	// 	$("#games").css("display", "block");
+	// }
+
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+	
+	// Next/previous controls
+	function plusSlides(n) {  
+	  showSlides(slideIndex += n);
+	  if(n==1){
+			var element = document.getElementById("list_"+parseInt(slideIndex));
+			element.classList.add("myrightAni");
+			element.classList.remove("myleftAni");
+		}
+		else{
+			var element = document.getElementById("list_"+parseInt(slideIndex));
+			element.classList.add("myleftAni");
+			element.classList.remove("myrightAni");
+		}
+	}
+	
+	// Thumbnail image controls
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	  if(n<1){
+		var element = document.getElementById("list_"+parseInt(slideIndex));
+		element.classList.add("myrightAni");
+		element.classList.remove("myleftAni");
+	}
+	else{
+		var element = document.getElementById("list_"+parseInt(slideIndex));
+		element.classList.add("myleftAni");
+		element.classList.remove("myrightAni");
+	}
+
+	}
+	
+	function showSlides(n) {  
+	  var i;
+	  var slides = document.getElementsByClassName("tab_content"); 
+	  var dots = document.getElementsByClassName("dot");
+	 
+	 if (n > slides.length) {slideIndex = 1}
+	 if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+		  slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+		  dots[i].className = dots[i].className.replace("active", "");
+		  slides[i].className = slides[i].className.replace("active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	  slides[slideIndex-1].className += " active";
+	}
+
+	$('.morebtn').click(function(){
+		$('.sliderBanners').addClass('fblur');
+	});
+	$(document).on('click',function(){ 
+		if($('#slidesPop').hasClass('sliderBanners')){
+			$('#slidesPop').removeClass('fblur'); 
+		}
+		else{
+			if($('.sliderBanners').hasClass('sliderBanners')){
+				$('#slidesPop').addClass('fblur'); 
+			}
+		}
+	});
+		$('.carousel').carousel({
+		interval: 2000
+	  });
+
